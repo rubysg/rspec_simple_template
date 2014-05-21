f2726a06b5fc242ed2ab2352954cc639c8140d10
Chong-Yee Khoo <mail@cykhoo.com>
2014-05-21 11:13:32 +0800

    initial commit

---
+++ changeme_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+
+describe "this is an example block" do
+
+  it "this is an example testing the presence of a class" do
+    expect(Changeme).to_not be_nil
+  end
+end
+
+# Uncomment the class definition to make the example pass
+#
+# class Changeme
+# end
